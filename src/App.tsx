9acd21e0c7572feb8b2e58716b4744b13ae4beb5
Yawar Ashraf <yawar1ashraf@gmail.com>
2025-08-18 11:59:24 -0400

    trying to see why website not showing up on custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,98 @@
 import './App.css'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import { Element } from 'react-scroll'
-import TutoringHero from './components/TutoringHero'
-import Navbar from './components/Navbar'
-import About from './components/About'
-import Features from './components/Features'
-import Pricing from './components/Pricing'
-import HowItWorks from './components/HowItWorks'
-import FAQ from './components/FAQ'
-import Stats from './components/Stats'
-import Hbreak from './components/Hbreak'
-import CTASection from './components/CTA'
-import Footer from './components/Footer'
-import Me from './components/Me'
-import Blog from './components/Blog'
-import NotesAndTests from './components/NotesTests'
-import BlogDetails from './components/BlogDetails';
+import { HashRouter} from 'react-router-dom'
+// import { HashRouter, Routes, Route } from 'react-router-dom'
+// import { Element } from 'react-scroll'
+// import TutoringHero from './components/TutoringHero'
+// import Navbar from './components/Navbar'
+// import About from './components/About'
+// import Features from './components/Features'
+// import Pricing from './components/Pricing'
+// import HowItWorks from './components/HowItWorks'
+// import FAQ from './components/FAQ'
+// import Stats from './components/Stats'
+// import Hbreak from './components/Hbreak'
+// import CTASection from './components/CTA'
+// import Footer from './components/Footer'
+// import Me from './components/Me'
+// import Blog from './components/Blog'
+// import NotesAndTests from './components/NotesTests'
+// import BlogDetails from './components/BlogDetails';
 
 
 
 function App() {
 
   return (
+
     <HashRouter>
-      <Navbar />
-      {/* Main content rendered via routes */}
-      <Routes>
-      <Route
-        path="/"
-        element={
-        <>
-          <Element name="home">
-          <TutoringHero />
-          </Element>
-          <Hbreak />
-          <Element name="about">
-          <About />
-          </Element>
-          <Hbreak />
-          <Element name="me">
-          <Me />
-          </Element>
-          <Hbreak />
-          <Element name="features">
-          <Features />
-          </Element>
-          <Hbreak />
-          <Element name="how-it-works">
-          <HowItWorks />
-          </Element>
-          <Hbreak />
-          <Element name="testimonials">
-          <Stats />
-          </Element>
-          <Hbreak />
-          <Element name="pricing">
-          <Pricing />
-          </Element>
-          <Hbreak />
-          <Element name="faq">
-          <FAQ />
-          </Element>
-          <Hbreak />
-          <CTASection />
-        </>
-        }
-      />
-      <Route
-        path="/notes-and-tests"
-        element={
-        <>
-          <NotesAndTests />
-        </>
-        }
-      />
-      <Route
-        path="/blog"
-        element={
-        <>
-          <Blog />
-        </>
-        }
-      />
-      <Route path="/blog/:id" 
-        element={<BlogDetails />} 
-      />
-      </Routes>
-      <Footer />
+      <h1>Hello Worlds</h1>
     </HashRouter>
+    // <HashRouter>
+    //   <Navbar />
+    //   {/* Main content rendered via routes */}
+    //   <Routes>
+    //   <Route
+    //     path="/"
+    //     element={
+    //     <>
+    //       <Element name="home">
+    //       <TutoringHero />
+    //       </Element>
+    //       <Hbreak />
+    //       <Element name="about">
+    //       <About />
+    //       </Element>
+    //       <Hbreak />
+    //       <Element name="me">
+    //       <Me />
+    //       </Element>
+    //       <Hbreak />
+    //       <Element name="features">
+    //       <Features />
+    //       </Element>
+    //       <Hbreak />
+    //       <Element name="how-it-works">
+    //       <HowItWorks />
+    //       </Element>
+    //       <Hbreak />
+    //       <Element name="testimonials">
+    //       <Stats />
+    //       </Element>
+    //       <Hbreak />
+    //       <Element name="pricing">
+    //       <Pricing />
+    //       </Element>
+    //       <Hbreak />
+    //       <Element name="faq">
+    //       <FAQ />
+    //       </Element>
+    //       <Hbreak />
+    //       <CTASection />
+    //     </>
+    //     }
+    //   />
+    //   <Route
+    //     path="/notes-and-tests"
+    //     element={
+    //     <>
+    //       <NotesAndTests />
+    //     </>
+    //     }
+    //   />
+    //   <Route
+    //     path="/blog"
+    //     element={
+    //     <>
+    //       <Blog />
+    //     </>
+    //     }
+    //   />
+    //   <Route path="/blog/:id" 
+    //     element={<BlogDetails />} 
+    //   />
+    //   </Routes>
+    //   <Footer />
+    // </HashRouter>
   )
 }
 
--- src/components/NotesButton.tsx
+++ src/components/NotesButton.tsx
@@ -7,8 +7,8 @@ interface NotesButtonProps {
 
 const NotesButton: React.FC<NotesButtonProps> = ({ onClick, children }) => (
   <Link
-    key="/tutoringWebsite/notes-and-tests"
-    to="/tutoringWebsite/notes-and-tests"
+    key="notes-and-tests"
+    to="notes-and-tests"
     className="px-6 py-2 bg-[#EBAD25] hover:bg-[#D99A22] text-black font-semibold rounded-xl shadow transition-colors duration-200 inline-block text-center"
     onClick={onClick}
   >
